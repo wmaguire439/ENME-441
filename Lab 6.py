00d12a0de214848d05baeb50c462fcd7649b762f
wmaguire439 <wmaguire@terpmail.umd.edu>
2025-10-21 00:16:18 -0400

    Add files via upload

--- Lab 6.py
+++ Lab 6.py
@@ -1,27 +1,43 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+class Shifter:
+    def __init__(self, serialPin, clockPin, latchPin):
+        # Store pin assignments as class attributes
+        self.serialPin = serialPin
+        self.clockPin = clockPin
+        self.latchPin = latchPin
 
-dataPin, latchPin, clockPin = 23,24,25
+        # GPIO setup
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.serialPin, GPIO.OUT)
+        GPIO.setup(self.clockPin, GPIO.OUT)
+        GPIO.setup(self.latchPin, GPIO.OUT)
 
-GPIO.setup(dataPin, GPIO.OUT)
-GPIO.setup(latchPin, GPIO.OUT, initial=0)
-GPIO.setup(clockPin, GPIO.OUT, initial=0)
+    def __ping(pin):
+        """Private method: create a short high pulse on a pin."""
+        GPIO.output(pin, 1)
+        time.sleep(0)
+        GPIO.output(pin, 0)
 
-pattern = 0b01100110
+   def shiftByte(b): # send a byte of data to the output
+		for i in range(8):
+			GPIO.output(dataPin, b & (1<<i))
+			ping(clockPin) # add bit to register
+		ping(latchPin) # send register to output
 
-for i in range(8):
-		GPIO.output(dataPin, pattern & (1<<i))
-		GPIO.output(clockPin, 1)
-		time.sleep(0)
-		GPIO.output(latchPin, 0)
+     
 
-GPIO.output(latchPin, 1)
-time.sleep(0)
-GPIO.output(latchPin, 0)
+# Example usage
+if __name__ == "__main__":
+    try:
+        shifter = Shifter(serialPin=23, clockPin=25, latchPin=24)
+        pattern = 0b01100110
+        shifter.shiftByte(pattern)
 
-try:
-	while 1: pass
-except:
-	GPIO.cleanup()
+        print("Pattern sent. Press Ctrl+C to exit.")
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        print("\nGPIO cleaned up. Exiting.")
