c966cb3bf43a7dc9a03a0963370790edd265df95
wmaguire439 <wmaguire@terpmail.umd.edu>
2025-10-21 10:34:40 -0400

    Add files via upload

--- shifter.py
+++ shifter.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 class Shifter:
-    def __init__(self, serialPin, clockPin, latchPin):
+	def __init__(self, serialPin, clockPin, latchPin):
         # Store pin assignments as class attributes
         self.serialPin = serialPin
         self.clockPin = clockPin
@@ -14,19 +14,19 @@ class Shifter:
         GPIO.setup(self.clockPin, GPIO.OUT)
         GPIO.setup(self.latchPin, GPIO.OUT)
 
-    def __ping(self, pin):
+	def __ping(self, pin):
         GPIO.output(pin, 1)
         time.sleep(0)
         GPIO.output(pin, 0)
 
-   	def shiftByte(self, pattern):
-   		for i in range(8):
-   			GPIO.output(self.dataPin, pattern & (1<<i))
+	def shiftByte(self, pattern):
+		for i in range(8):
+			GPIO.output(self.dataPin, pattern & (1<<i))
 			GPIO.output(self.clockPin, 1)
-        	time.sleep(0)
-       		GPIO.output(self.clockPin, 0) # add bit to register
+			time.sleep(0)
+			GPIO.output(self.clockPin, 0) # add bit to register
 		GPIO.output(self.latchPin, 1)
         time.sleep(0)
-       	GPIO.output(self.latchPin, 0) # send register to output
+		GPIO.output(self.latchPin, 0) # send register to output
 
      
